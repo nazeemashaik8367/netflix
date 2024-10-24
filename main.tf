5892b5462e0a2e5894bfda5d9b8694b8de051a42
nazeemashaik8367 <naziya1016@gmail.com>
2024-10-24 14:57:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "ca-central-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-0ddc798b3f1a5117e"
+instance_type = "t2.medium"
+key_name = "rahull"
+vpc_security_groups_ids = ["sg-0cd95bc0c6ae67b9a"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
+}
